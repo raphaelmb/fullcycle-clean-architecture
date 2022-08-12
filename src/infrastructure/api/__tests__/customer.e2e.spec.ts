bf3db4c02bae663375ee647e9c1a9b69eee69113
Raphael Berger <raphaelmb@gmail.com>
2022-08-12 14:28:25 -0300

    Add customer list endpoint

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -1,5 +1,6 @@
 import { app, sequelize } from "../express";
 import request from "supertest";
+import e from "express";
 
 describe("E2E test for customer", () => {
   beforeEach(async () => {
@@ -29,5 +30,39 @@ describe("E2E test for customer", () => {
       name: "John",
     });
     expect(response.status).toBe(500);
+  });
+
+  it("should list all customers", async () => {
+    const customer1 = await request(app)
+      .post("/customer")
+      .send({
+        name: "John",
+        address: {
+          street: "Street 1",
+          number: 123,
+          zip: "12345",
+          city: "City 1",
+        },
+      });
+
+    const customer2 = await request(app)
+      .post("/customer")
+      .send({
+        name: "Jane",
+        address: {
+          street: "Street 2",
+          number: 321,
+          zip: "54321",
+          city: "City 2",
+        },
+      });
+
+    const response = await request(app).get("/customer").send();
+    expect(response.status).toBe(200);
+    expect(response.body.customers.length).toBe(2);
+    expect(response.body.customers[0].name).toBe("John");
+    expect(response.body.customers[0].address.street).toBe("Street 1");
+    expect(response.body.customers[1].name).toBe("Jane");
+    expect(response.body.customers[1].address.street).toBe("Street 2");
   });
 });
--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
+import ListCustomerUseCase from "../../../usecase/customer/list/list.customer.usescase";
 import CustomerRepository from "../../customer/repository/sequelize/customer.repository";
 
 export const customerRoute = express.Router();
@@ -18,6 +19,16 @@ customerRoute.post("/", async (req: Request, res: Response) => {
     };
     const output = await usecase.execute(customerDto);
     res.status(201).send(output);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+customerRoute.get("/", async (req: Request, res: Response) => {
+  const usecase = new ListCustomerUseCase(new CustomerRepository());
+  try {
+    const output = await usecase.execute({});
+    res.send(output);
   } catch (error) {
     res.status(500).send(error);
   }
